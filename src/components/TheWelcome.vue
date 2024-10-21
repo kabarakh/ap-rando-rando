a98d495c211f37529432caf8670bdd6e89dc3144
Christian Keuerleber <keuerleber@punkt.de>
2024-10-21 15:00:48 +0200

    tryout weighted rando, include lodash, use non-decorator orm stuff

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { map, shuffle } from 'lodash';
+import { chain, map, random, shuffle, uniq } from 'lodash';
 import { parse } from 'yaml';
 import { ref } from 'vue';
 import { useRepo } from 'pinia-orm'
@@ -8,25 +8,38 @@ import Template from '@/models/template'
 const templatesRepo = useRepo(Template);
 
 const saveTemplatesFromFiles = () => {
-  templates.forEach((template) => {
+  const templateFiles = import.meta.glob('@/assets/templates/*.yaml', {
+    query: '?raw',
+    import: 'default',
+    eager: true,
+  });
+
+  const fileTemplates = map(templateFiles, module => {
+    return parse(module);
+  });
+
+  fileTemplates.forEach((template) => {
     templatesRepo.save({content: template})
   })
 }
 
-const templateFiles = import.meta.glob('@/assets/templates/*.yaml', {
-    query: '?raw',
-    import: 'default',
-    eager: true,
-});
-
-const templates = map(templateFiles, module => {
-    return parse(module);
-});
-
 const numberOfWorlds = ref(4);
 const chosenWorlds = ref([]);
 
+const templates = templatesRepo.all();
+
 const chooseWorlds = () => {
+  const newTemplates = [];
+  const randomCount = random(200, 300);
+  const newArray = Array(randomCount).fill(templates[0]);
+  const randomCount2 = random(10, 30);
+  const newArray2 = Array(randomCount2).fill(templates[1]);
+   const concatArray = newTemplates.concat(templates, newArray, newArray2)
+  console.log(concatArray)
+  const newShuffled = shuffle(concatArray)
+    const uniqShuffled = uniq(newShuffled)
+console.log(newShuffled, uniqShuffled, uniqShuffled.slice(0, numberOfWorlds.value))
+
     const shuffled = shuffle(templates);
     chosenWorlds.value = shuffled.slice(0, numberOfWorlds.value);
 };
@@ -34,7 +47,7 @@ const chooseWorlds = () => {
 
 <template>
   <button @click.prevent="saveTemplatesFromFiles">Save templates from files</button>
-    <p>{{ templatesRepo.all().length }} yaml template files loaded</p>
+    <p>{{ templates.length }} yaml template files loaded</p>
     <form @submit.prevent="chooseWorlds">
         <input
             type="number"
@@ -47,7 +60,7 @@ const chooseWorlds = () => {
     </form>
     <ul>
         <li v-for="(template, index) in chosenWorlds" :key="index">
-            {{ template.game }}
+            {{ template.content.game }}
         </li>
     </ul>
 </template>
--- src/models/template.ts
+++ src/models/template.ts
@@ -1,12 +1,16 @@
 import { Model } from 'pinia-orm'
-import { Cast, Uid, Attr } from 'pinia-orm/decorators'
-import { ArrayCast } from 'pinia-orm/casts'
 
 export default class Template extends Model {
   static entity = 'templates'
 
-  @Uid() declare id: string;
-  @Attr('{}') @Cast(() => ArrayCast) declare content: Record<string, any>
+  static fields() {
+    return {
+      id: this.uid(),
+      content: this.attr({})
+    }
+  }
+  declare id: string;
+  declare content: {[key: string]: any}
 
   static piniaOptions = {
     persist: true
